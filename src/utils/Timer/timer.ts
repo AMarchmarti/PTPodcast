4144443613f0fcadadc3b9449c6d30e17968b89b
AMarchmarti <marchmarti.antoni@gmail.com>
2024-05-31 14:25:40 +0200

    feature[presentation]: added useCache and util timer

---
+++ src/utils/Timer/timer.ts
@@ -0,0 +1,14 @@
+ enum TimerType { 
+    SECOND = 1,
+    MINUTE = 60,
+    HOUR = 3600
+
+}
+
+ const getTimer = (time: number, type: TimerType) => { 
+    return time * type * 1000
+}
+export {
+    TimerType,
+    getTimer
+} 
