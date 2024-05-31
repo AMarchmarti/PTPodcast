dc598297707d582813cd500218e3457943988f22
AMarchmarti <marchmarti.antoni@gmail.com>
2024-05-31 14:26:04 +0200

    feature[presentation]: create basic pages

---
+++ src/presentation/pages/EpisodeListPage/EpisodeList.page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EpisodeListPage = () => {
+  return (
+    <div>EpisodeList.page</div>
+  )
+}
---
+++ src/presentation/pages/EpisodePage/Episode.page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const EpisodePage = () => { 
+    return (
+        <div>
+            <h1>Episode Page</h1>
+        </div>
+    )
+}
