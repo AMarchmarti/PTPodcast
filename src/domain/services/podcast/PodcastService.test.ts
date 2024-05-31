6f962c2e75a574e3de39ea78731c1195da8b14fb
AMarchmarti <marchmarti.antoni@gmail.com>
2024-05-31 12:00:54 +0200

    feature[fetch]: create test for endpoints and fix some globals

---
+++ src/domain/services/podcast/PodcastService.test.ts
@@ -0,0 +1,31 @@
+import { mockPodcastResponse } from "@/domain/mocks/Podcast/PodcastMock";
+
+import { PodcastService } from "@/domain/services/podcast/PodcastService";
+import { afterEach } from "node:test";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+describe("PodcastService", () => {
+  let podcastService: PodcastService;
+
+  beforeEach(() => {
+    podcastService = new PodcastService();
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should fetch all podcasts", async () => {
+    // Mock the HTTP request and response
+   
+    vi.spyOn(podcastService, "getAllPodcasts").mockResolvedValue(
+      mockPodcastResponse
+    );
+
+    // Call the method under test
+    const result = await podcastService.getAllPodcasts();
+
+    // Assert the result
+    expect(result).toEqual(mockPodcastResponse);
+  });
+});
